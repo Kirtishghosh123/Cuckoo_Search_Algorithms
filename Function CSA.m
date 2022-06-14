61435b07fd7cdeb5fd401100cba3e625576a1674
Kirtishghosh123 <102647573+Kirtishghosh123@users.noreply.github.com>
2022-06-14 13:12:06 +0530

    Update Function CSA.m

--- Function CSA.m
+++ Function CSA.m
@@ -1,41 +1,36 @@
 format short
-clear all;
-clc;
----------------------------------------------
+clear all
+clc
 
-%% Pharse  : Input Parameter
-D = 3;
-lb = [-1 -1 -1];
-ub = [1 1 1];
-N = 10;
-n = N;
-pa = 0.25
-max_iter =100;
-----------------------------------------------
+%% Pharse 1 : Input Parameter
 
-%% Pharse 2 : Defining Objective Function
-  
-function out = fns(X)
-    x = X(:,1);
-    y = X(:,2);
-    z = X(:,3);
-    out = (sin(x)./x).*(sin(y)./y).*(sin(z)./z); 
-end
-=---------------------------------------------------
+D = 2; %Dimension fo the problem
+lb = [-5, -5]; %Lower Bound of the variable
+ub = [5 5]; %Upper bound of the variable
+N = 20; %Population Size
+n = N; 
+pa = 0.45; %Discovery Rate of the alien eggs and solutions
+max_iter = 100; %Maximum number of iteration
 
-%% Pharse 3 : Generate Initial Population Range
+
+
+%% Pharse 3 : Generate Initial Population
+
 for i = 1 : N
     for j = 1 : D
-        nest(i, j) = lb(:,j) + rand.*(ub(:,j) - lb(:,j))
+        nest(i, j) = lb(:,j) + rand.*(ub(:,j) - lb(:,j));
     end
 end
-fx = fns(nest);
 
+fx =  fns(nest);
 beta = 3/2;
-sigma = (gamma(1 + beta)*sin(pi*beta/2)/(gamma((1 + beta)/2)*beta*2^((beta-1)/2)))^(1/beta);
--------------------------------------------------------------------------------------------------------
+sigma = (gamma(1 + beta)*sin(pi*beta/2)/(gamma((1 + beta)/2)*beta*2^((beta - 1)/2)))^(1/beta);
 
-for iter = 1 : max_iter
+
+
+%% Pharse 4 : Cuckoo Search Main Loop Search
+
+for iter = 3 : max_iter
     [fnv, indf] = min(fx);
     best = nest(indf, :);
     
@@ -47,7 +42,7 @@ for iter = 1 : max_iter
         v = randn(size(s));
         step = u./abs(v).^(1/beta);
         Xnew = X + randn(size(s)).*0.01.*step.*(X - best);
-        
+
         %%Check Bounds
         for kk = 1: size(Xnew, 2)
             if Xnew(kk) > ub(kk)
@@ -65,56 +60,50 @@ for iter = 1 : max_iter
         end
     end
 
-end
-------------------------------------------------------------------------
 
-%% Find the current best
-[fmin, K1] = min(fx);
-best = nest(K1, :);
+    [fmin,K1] = min(fx);
+    best =  nest(K1, :);
 
--------------------------------------------------------------------------
+    %% Replace some nest by constructing new solution
 
-%% Replace Some nest by constructing new solution
-K = rand(size(nest)) < pa;
+    K = rand(size(nest)) < 0.25;
+    stepsizeK = rand*(nest(randperm(n), :) - nest(randperm(n), :));
+    new_nest = nest + stepsizeK.*K;
+    
 
-stepsizeK = rand*(nest(randperm(n), :) - nest(randperm(n), :));
-new_nest = nest + stepsizeK.*K;
------------------------------------------------------------------------
 
-%% Check Bounds
-for ii = 1 : size(nest, 1)
-    s =new_nest(ii, :);
-    for kk = 1 : size(s, 2)
-        if s(kk) > ub(kk)
-            s(kk) = ub(kk);
-        elseif s(kk) < lb(kk)
-            s(kk) = lb(kk);
+    for ii = 1 : size(nest, 1)
+        s =new_nest(ii, :);
+        for kk = 1 : size(s, 2)
+            if s(kk) > ub(kk)
+                s(kk) = ub(kk);
+            elseif s(kk) < lb(kk)
+                s(kk) = lb(kk);
+            end
+        end
+        new_nest(ii,:) = s; 
+        %% Perform Greedy Selection
+        fnew = fns(s);
+        if fnew < fx(ii, :)
+            nest(ii, :) = s;
+            fx(ii, :) = fnew;
         end
     end
-    new_nest(ii,:) = s;
--------------------------------------------------------------------------
 
-%% Perform Greedy Selection
-    fnew = fns(s);
-    if fnew < fx(ii, :)
-        nest(ii, :) = s;
-        fx(ii, :) = fnew;
-    end
+    %% Finding the optimal Value
+    [optval, optind] = min(fx);
+
+    BestFx(iter) = optval;
+    BestX(iter, :) = nest(optind, :);
+    
+    disp(['Iteration' num2str(iter) ' : Best Cost = ' num2str(BestFx(iter)) ]);
+    
+    %% Plotting the result
+    plot(BestFx, 'LineWidth', 2);
+    xlabel('Iteration Number')
+    ylabel('Fitness Value')
+    title('Convergence Vs Iteration')
+    grid on
+    
 end
-
-
-%% Finding the optimal Value
-[optval, optind] = min(fx);
-
-BestFx(iter) = optval;
-BestX(iter, :) = nest(optind, :);
-
-
-%% Plotting the result
-plot(BestFx, 'LineWidth', 2);
-xlabel('Iteration Number')
-ylabel('Fitness Value')
-title('Convergence Vs Iteration')
-grid on
-
 
